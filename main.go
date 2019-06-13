681f68a7d6e72ff5cebd831e7f6e26bffafcce39
Salvador Guzman <guzmansalv@gmail.com>
2019-06-13 04:12:46 -0700

    done - working key server and registry

--- main.go
+++ main.go
@@ -10,10 +10,18 @@ import (
 )
 
 func handleConnection(c net.Conn) {
-	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
+	defer func() {
+		err := c.Close()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(4)
+		}
+	}()
+
+	key := getRandomKey()
+	fmt.Println("Serving", c.RemoteAddr().String(), key)
 	{
-		result := "hello\n"
-		_, err := c.Write([]byte(string(result)))
+		_, err := c.Write([]byte(key))
 		{
 			if err != nil {
 				fmt.Println(err)
@@ -21,16 +29,20 @@ func handleConnection(c net.Conn) {
 			}
 		}
 	}
-	err := c.Close()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(4)
-	}
 }
+
+type LengthType = uint8
 
 const (
 	port = "0.0.0.0:3333"
-	google = "https://www.googleapis.com/youtube/v3/channels?part=statistics&key=UC-lHJZR3Gqxm24_Vd_AJ5Yw&id=%s"
+	sleepTime = 60
+	google = "https://www.googleapis.com/youtube/v3/channels?part=id&id=UC-lHJZR3Gqxm24_Vd_AJ5Yw&key=%s"
+)
+
+var (
+	keys []string
+	status []bool
+	length LengthType
 )
 
 func checkKey(key string) bool {
@@ -53,36 +65,63 @@ func checkKey(key string) bool {
 	return status
 }
 
-func init() {
-	rand.Seed(time.Now().Unix())
-	fmt.Println("Key service started")
+func keyAudit() {
+	fmt.Println("Key audit started")
+
+	for {
+		for i := LengthType(0); i < length; i++ {
+			time.Sleep(sleepTime * time.Second)
+			key := keys[i]
+			cond := checkKey(key)
+
+			fmt.Println(key, cond)
+			status[i] = cond
+		}
+	}
 }
 
-func main() {
-	args := os.Args[1:]
+func getRandomKey() string {
+	goodKeys := make([]string, 0)
+	for i := LengthType(0); i < length; i++ {
+		if status[i] {
+			goodKeys = append(goodKeys, keys[i])
+		}
+	}
+
+	if len(goodKeys) == 0 {
+		fmt.Println("No good keys right now")
+		os.Exit(6)
+	}
+
+	n := rand.Intn(len(goodKeys))
+
+	return goodKeys[n]
+}
+
+func init() {
+	fmt.Println("Key service started")
+	rand.Seed(time.Now().Unix())
+
+	keys = os.Args[1:]
+	length = LengthType(len(keys))
 	{
-		if len(args) == 0 {
+		if length == 0 {
 			fmt.Println("No keys")
 			os.Exit(1)
 		}
 
-		fmt.Println("Received", args)
+		fmt.Println("Received", keys)
 	}
 
-	keys := make([]string, 0)
+	status = make([]bool, length)
 	{
-		for _, key := range args {
-			if checkKey(key) {
-				keys = append(keys, key)
-			}
-		}
-
-		if len(keys) == 0 {
-			fmt.Println("No good keys")
-			os.Exit(5)
+		for i := LengthType(0); i < length; i++ {
+			status = append(status, checkKey(keys[i]))
 		}
 	}
+}
 
+func main() {
 	server, err := net.Listen("tcp4", port)
 	{
 		if err != nil {
@@ -94,6 +133,7 @@ func main() {
 	defer func() {
 		_ = server.Close()
 	}()
+	go keyAudit()
 
 	for {
 		fmt.Println("Waiting for connection")
