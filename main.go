f245f21b1c741ab4d78f46df8780ae2562303776
Salvador Guzman <guzmansalv@gmail.com>
2019-06-13 07:18:51 -0700

    first draft

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"./message"
 	"database/sql"
-	"encoding/binary"
 	"fmt"
 	"github.com/golang/protobuf/proto"
 	"math/rand"
@@ -15,101 +14,65 @@ import (
 )
 
 type ChannelRow struct {
+	time time.Time
 	id int32
-	serial string
+	sub int32
 }
 
 const (
-	port = "0.0.0.0:3334"
-	sleepTime = 3600
+	port = "0.0.0.0:3335"
 	sqlUrl = "postgresql://admin@localhost:5432/youtube?sslmode=disable"
-	sqlQuery = "SELECT id, serial FROM youtube.stats.channels ORDER BY id ASC"
+	sqlInsert = "INSERT INTO youtube.stats.channels (time, id, subs) VALUE ($1, $2, $3)"
+	cacheSize = 1000
 )
 
 var (
-	rows []ChannelRow
+	cache []ChannelRow
 )
 
-func getRandomRows(limit int) []byte {
-	var top int
-	if limit > len(rows) {
-		top = len(rows)
-	}
-
-	n := 50
-	// record indexes here to prevent duplicates
-	indexes := make(map[int]bool)
-
-	// create n random indexes
-	for i := 0; i < n; i++ {
-		var r int
-		for {
-			r = rand.Intn(top)
-			if indexes[r] {
-				continue
-			}
-			break
-		}
-
-		indexes[r] = true
-	}
-
-	var protoMsg message.ChannelMessage
-	for i := range indexes {
-		protoMsg.Ids = append(protoMsg.Ids, rows[i].id)
-		protoMsg.Serials = append(protoMsg.Serials, rows[i].serial)
-	}
-
-	fmt.Println("Sending", protoMsg)
-
-	data, err := proto.Marshal(&protoMsg)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	return data
-}
-
 func handleConnection(c net.Conn) {
-	defer func() {
-		err := c.Close()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(4)
-		}
-	}()
-
-	bytes := make([]byte, 4)
-	n, err := c.Read(bytes)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(7)
-	}
-
-	if n != 4 {
-		fmt.Println("Need to read 4 bytes - received", n)
-		return
-	}
-
-	fmt.Println("Retrieved", n, "bytes")
-	limit := int(binary.LittleEndian.Uint32(bytes))
-	fmt.Println("Limit is", limit)
-
-	protoBytes := getRandomRows(limit)
+	var bytes []byte
 	{
-		_, err := c.Write(protoBytes)
 		{
+			n, err := c.Read(bytes)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(7)
+			}
+
+			fmt.Println("Retrieved", n, "bytes")
+		}
+
+		{
+			err := c.Close()
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(4)
 			}
 		}
 	}
+
+	msg := &message.ChannelMessage{}
+	{
+		err := proto.Unmarshal(bytes, msg)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	for i := 0; i < len(msg.Ids); i++ {
+		time := time.Now()
+		id := msg.Ids[i]
+		sub := msg.Subs[i]
+
+		row := ChannelRow{time, id, sub}
+		cache = append(cache, row)
+	}
 }
 
-func setChannels() {
-	fmt.Println("Updating channels")
-
+func write() {
+	fmt.Println("Writing", cacheSize, "channels")
 	db, err := sql.Open("postgres", sqlUrl)
 	if err != nil {
 		fmt.Println(err)
@@ -121,41 +84,36 @@ func setChannels() {
 		_ = db.Close()
 	}()
 
-	results, err := db.Query(sqlQuery)
+	txn, err := db.Begin()
 	if err != nil {
 		fmt.Println(err)
-		os.Exit(6)
+		os.Exit(8)
 	}
 
-	tmp := make([]ChannelRow, 0)
-	var row ChannelRow
-
-	for results.Next() {
-		err := results.Scan(&row.id, &row.serial)
+	defer func() {
+		err = txn.Commit()
 		if err != nil {
 			fmt.Println(err)
-			os.Exit(7)
+			os.Exit(9)
 		}
-		tmp = append(tmp, row)
+	}()
+
+	for i := 0; i < cacheSize; i++ {
+		row := cache[i]
+		_, err := txn.Exec(sqlInsert, row.time, row.id, row.sub)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(6)
+		}
 	}
 
-	fmt.Println("Retrieved", len(tmp), "channels")
-	rows = tmp
-}
-
-func channelUpdate() {
-	for {
-		fmt.Println("Waiting for", sleepTime, "seconds until repulling channels")
-		time.Sleep(sleepTime * time.Second)
-		setChannels()
-	}
+	cache = cache[cacheSize:]
+	fmt.Println("New size of cache", len(cache))
 }
 
 func init() {
 	fmt.Println("Cache service started")
 	rand.Seed(time.Now().Unix())
-
-	setChannels()
 }
 
 func main() {
@@ -170,8 +128,6 @@ func main() {
 	defer func() {
 		_ = server.Close()
 	}()
-
-	go channelUpdate()
 
 	for {
 		fmt.Println("Waiting for connection")
