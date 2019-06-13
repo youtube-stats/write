9000f41ead4734892ff68733223f08a35e9e3cc9
Salvador Guzman <guzmansalv@gmail.com>
2019-06-13 02:58:47 -0700

    simple args

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Key service started")
+
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("No keys")
+		os.Exit(1)
+	}
+
+	fmt.Print("Received")
+	fmt.Print(args)
 }
